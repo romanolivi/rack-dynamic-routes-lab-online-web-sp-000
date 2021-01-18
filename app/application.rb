5762c594b42de17e3556db20217307376e70ebfa
Romano Livi <romano_livi@outlook.com>
2021-01-18 22:59:07 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application 
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name
+      
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
+      
